960692f30b4940523bb82681a90b97e446d92976
mnoorfawi <m.noor.fawi@gmail.com>
2020-10-14 05:14:00 +0400

    structpie v0.0.2

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ with open("requirements.txt") as rq:
 
 setup(
       name="structpie",
-      version="0.0.1",
+      version="0.0.2",
       description="A set of python modules for data structures written in C",
       packages=["structpie"],
       install_requires=requirements,
