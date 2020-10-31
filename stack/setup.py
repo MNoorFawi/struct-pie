b3b4e0410b16b69ae52fc749e93911bfd57bdfcb
mnoorfawi <m.noor.fawi@gmail.com>
2020-10-31 10:34:34 +0400

    updating directories

--- stack/setup.py
+++
@@ -1,7 +0,0 @@
-from distutils.core import setup
-from distutils.extension import Extension
-from Cython.Distutils import build_ext
-
-ext_modules = [Extension('pystack', ['pystack.pyx', 'stack.c'])]
-
-setup(cmdclass={'build_ext': build_ext}, ext_modules=ext_modules)
