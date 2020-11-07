38649f6e3dc91edcfe67d522319c4eebd883ef27
mnoorfawi <m.noor.fawi@gmail.com>
2020-11-07 13:23:07 +0400

    hash table with binary search tree

--- hash_table_bstree/setup.py
+++ hash_table_bstree/setup.py
@@ -2,15 +2,21 @@ from distutils.core import setup
 from distutils.extension import Extension
 from Cython.Distutils import build_ext
 import os
+import sys
+
 
 os.system("gcc -fPIC -c bstree.c -o bstree.o")
-os.system("gcc -shared bstree.o -o libbstree.so")
-os.system("export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:./")
+if sys.platform == "linux":
+    os.system("gcc -shared bstree.o -o libbstree.so")
+    os.system("export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:./")
+elif "win" in sys.platform:
+    os.system("gcc -shared bstree.o -o libbstree.dll")
+    os.system('PATH="$PATH:./"')
 
 ext_modules = [Extension("hash_bst", ["pyhash.pyx", "hash.c", "bstree.c"])]
 
 setup(
-  cmdclass = {'build_ext': build_ext},
+  cmdclass = {"build_ext": build_ext},
   ext_modules = ext_modules
   )
 
